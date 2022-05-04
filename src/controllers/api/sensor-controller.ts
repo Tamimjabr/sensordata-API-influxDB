a3e075e7378c4126d2f0401e1379760adbae01f7
Tamim Jabr <tj222kg@student.lnu.se>
2022-05-04 17:22:34 +0200

    get sensor data from DB

--- src/controllers/api/sensor-controller.ts
+++ src/controllers/api/sensor-controller.ts
@@ -1,17 +1,19 @@
 import createError from 'http-errors';
 import { NextFunction, Request, Response } from 'express';
-import { saveSensorData } from '../../respository/sensor-repository';
+import { getSensorData, saveSensorData } from '../../respository/sensor-repository';
 
 
 
 export class SensorController {
 
-
   async getAll (req: Request, res: Response, next: NextFunction) {
     try {
-      await saveSensorData()
+      // await saveSensorData()
+      const data = await getSensorData()
+      console.log('inside controller')
       res.status(200).send({
         status: 'success',
+        data
       })
     } catch (error: any) {
       if (error.code === 'ENOTFOUND') {
--- src/respository/sensor-repository.ts
+++ src/respository/sensor-repository.ts
@@ -2,10 +2,10 @@ import { Point } from '@influxdata/influxdb-client';
 import influxDB from "../config/influxDB"
 
 const client = influxDB.getInstance()
+let org = `tamimjabr1995@gmail.com`
+let bucket = `SensorData`
 
 export const saveSensorData = async () => {
-  let org = `tamimjabr1995@gmail.com`
-  let bucket = `SensorData`
 
   let writeClient = await client.getWriteApi(org, bucket, 'ns')
 
@@ -18,4 +18,19 @@ export const saveSensorData = async () => {
     const response = await writeClient.flush()
   }
 
+}
+//  |> range(start: -30m)
+
+export const getSensorData = async () => {
+  const queryApi = client.getQueryApi(org)
+
+  const query = `option v = {timeRangeStart: -1h, timeRangeStop: now()}
+  
+  from(bucket: "SensorData")
+  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+  |> filter(fn: (r) => r["_measurement"] == "measurement2")
+  |> filter(fn: (r) => r["_field"] == "field1")
+  |> filter(fn: (r) => r["tagname10"] == "tagvalue1")`
+
+  return await queryApi.collectRows(query)
 }
